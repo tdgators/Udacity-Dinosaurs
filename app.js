9b1b7f2dff37d1d3c83d8d1d6b867b0e40e920f4
Tom <tvdonnelly@gmail.com>
2021-12-12 23:49:50 +0000

    Basic start.

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 
     // Create Dino Constructor
 
+    function Dino(name, height, weight, diet) {
+      this.name = name;
+      this.height = height;
+      this.weight = weight;
+      this.diet = diet;
+
+      //this.identify = function () {
+      //  console.log(`${this.name}'s population is ${this.population}.`);
+      //};
+    }
 
     // Create Dino Objects
+
+    const myDino = new Dino('Tom', 72, 185, 'Omnivore');
+
+    console.log(myDino);
+    // {
+    //   name: 'Mountain View',
+    //   population: 78000,
+    //   identify: function () {
+    //     console.log(`${this.name}'s population is ${this.population}.`);
+    //   };
+    // }
 
 
     // Create Human Object
@@ -11,19 +32,19 @@
 
 
     // Create Dino Compare Method 1
-    // NOTE: Weight in JSON file is in lbs, height in inches. 
+    // NOTE: Weight in JSON file is in lbs, height in inches.
 
-    
+
     // Create Dino Compare Method 2
     // NOTE: Weight in JSON file is in lbs, height in inches.
 
-    
+
     // Create Dino Compare Method 3
     // NOTE: Weight in JSON file is in lbs, height in inches.
 
 
     // Generate Tiles for each Dino in Array
-  
+
         // Add tiles to DOM
 
     // Remove form from screen
