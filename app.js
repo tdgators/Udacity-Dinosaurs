609aa4fb838040bfc9a0f445a1e7749b14a595c3
Tom <tvdonnelly@gmail.com>
2021-12-14 04:51:18 +0000

    App.js fixed function typo.

--- app.js
+++ app.js
@@ -80,7 +80,7 @@
     // non-async/await, promise-based fetch for json data.
     const fetchDinoData = function fetchDinoData() {
       fetch('dino.json')
-      .then(response => response.json())
+      .then(function (response) { response.json() })
       .then(function(data) {
         console.log("fetch data: " + data);
         if (data.Dinos) {
